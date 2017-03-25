1bbb5742e0cc31a113fa6d72d66fa4261885bd59
divyanshu <divyanshu.r46956@gmail.com>
2017-03-26 04:41:18 +0530

    Converted unreviewedsession factory to ES6 service class

---
+++ public/sessions/unreviewedSessionCount.ts
@@ -0,0 +1,19 @@
+angular.module('app').service('unreviewedSessionCount', class unsession {
+  
+	  sessions:any;
+	  currentIdentity:any;
+  	value:number = 0;
+
+  	constructor(sessions, currentIdentity){
+  		this.sessions = sessions;
+  		this.currentIdentity = currentIdentity;
+  	}
+
+    updateUnreviewedSessionCount() {
+      this.sessions.getUnreviewedCount(this.currentIdentity.currentUser.id)
+          .then((response) => {
+        this.value = response.data.count;
+      })
+    }
+
+})
